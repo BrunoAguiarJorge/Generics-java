82f72ddfa0f316df958ebf7d8b38b86a58b35d0b
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-09-23 14:49:26 +0100

    printservice integer

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,24 +4,26 @@ import java.util.Scanner;
 
 import services.PrintService;
 
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		PrintService ps = new PrintService();
+		PrintService<Integer> ps = new PrintService<>();
 
 		System.out.print("How many values? ");
 		int n = sc.nextInt();
 
 		for (int i = 0; i < n; i++) {
-			int value = sc.nextInt();
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
 
 		ps.print();
-		System.out.println("First: " + ps.first());
+		Integer x = ps.first();
+		System.out.print("First: " + x);
 
 		sc.close();
 	}
